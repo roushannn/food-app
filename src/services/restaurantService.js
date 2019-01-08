a38e7d560e09f95de2c35c41763e24816fcb6446
Angeline <atan@thoughtworks.com>
2019-01-08 16:39:54 +0800

    Add image url for restaurant in sample data

--- src/services/restaurantService.js
+++ src/services/restaurantService.js
@@ -5,7 +5,9 @@ const restaurants = [
     address: "35 Boat Quay, 049824 Singapore",
     openingTime: "11:00 AM",
     closingTime: "10:30 PM",
-    cusine: { _id: "5c3430ecfc13ae122d000000", name: "Western" }
+    cusine: { _id: "5c3430ecfc13ae122d000000", name: "Western" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000000.jpg"
+
   },
   {
     _id: "5c342ac9fc13ae39f8000001",
@@ -13,7 +15,8 @@ const restaurants = [
     address: "120 Maxwell Road, Tanjong Pagar Xchange, B1-31, 069119 Singapore",
     openingTime: "12:00 PM",
     closingTime: "9:30 PM",
-    cusine: { _id: "5c3430ecfc13ae122d000000", name: "Western" }
+    cusine: { _id: "5c3430ecfc13ae122d000000", name: "Western" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000001.jpg"
   },
   {
     id: "5c342ac9fc13ae39f8000002",
@@ -21,7 +24,8 @@ const restaurants = [
     address: "133 New Bridge Road, #B1-37 Chinatown Point, 059413 Singapore",
     openingTime: "10:00 AM",
     closingTime: "9:30 PM",
-    cusine: { _id: "5c3430ecfc13ae122d000000", name: "Western" }
+    cusine: { _id: "5c3430ecfc13ae122d000000", name: "Western" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000002.jpg"
   },
   {
     _id: "5c342ac9fc13ae39f8000003",
@@ -30,7 +34,8 @@ const restaurants = [
       "#03-89, 6 Eu Tong Sen Street, The Central, Clarke Quay, Lower Central, 059817 Singapore",
     openingTime: "11:00 AM",
     closingTime: "10:00 PM",
-    cusine: { _id: "5c3430ecfc13ae122d000001", name: "Japanese" }
+    cusine: { _id: "5c3430ecfc13ae122d000001", name: "Japanese" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000003.jpg"
   },
   {
     _id: "5c342ac9fc13ae39f8000004",
@@ -39,7 +44,8 @@ const restaurants = [
       "B1-24/25, 1 Raffles Place, OUB Centre, Clarke Quay, Lower Central, 048616 Singapore",
     openingTime: "11:00 AM",
     closingTime: "8:00 PM",
-    cusine: { _id: "5c3430ecfc13ae122d000001", name: "Japanese" }
+    cusine: { _id: "5c3430ecfc13ae122d000001", name: "Japanese" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000004.jpg"
   },
   {
     _id: "5c342ac9fc13ae39f8000005",
@@ -47,7 +53,8 @@ const restaurants = [
     address: "1 Lor Telok, River Valley, West Central, 049014 Singapore",
     openingTime: "11:30 AM",
     closingTime: "10:30 PM",
-    cusine: { _id: "5c3430ecfc13ae122d000002", name: "Thai" }
+    cusine: { _id: "5c3430ecfc13ae122d000002", name: "Thai" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000005.jpg"
   },
   {
     _id: "5c342ac9fc13ae39f8000006",
@@ -56,7 +63,8 @@ const restaurants = [
       "01-47, Telok Ayer Street, Amoy Street Food Centre, Tanjong Pagar, 069111 Singapore",
     openingTime: "11:30 AM",
     closingTime: "8:00 PM",
-    cusine: { _id: "5c3430ecfc13ae122d000002", name: "Thai" }
+    cusine: { _id: "5c3430ecfc13ae122d000002", name: "Thai" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000006.jpg"
   },
   {
     _id: "5c342ac9fc13ae39f8000007",
@@ -65,7 +73,8 @@ const restaurants = [
       "Stall 32, 7 Maxwell Road, Amoy Street Food Centre, Lower Central, 069111 Singapore",
     openingTime: "10:00 AM",
     closingTime: "7:00 PM",
-    cusine: { _id: "5c3430ecfc13ae122d000003", name: "Chinese" }
+    cusine: { _id: "5c3430ecfc13ae122d000003", name: "Chinese" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000007.jpg"
   },
   {
     _id: "5c342ac9fc13ae39f8000008",
@@ -73,7 +82,8 @@ const restaurants = [
     address: "6 North Canal Road, Clarke Quay, Lower Central, 048819 Singapore",
     openingTime: "11:00 AM",
     closingTime: "8:30 PM",
-    cusine: { _id: "5c3430ecfc13ae122d000003", name: "Chinese" }
+    cusine: { _id: "5c3430ecfc13ae122d000003", name: "Chinese" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000008.jpg"
   },
   {
     _id: "5c342ac9fc13ae39f8000009",
@@ -82,7 +92,8 @@ const restaurants = [
       "#02-36, 133 New Bridge Road, Chinatown Point, Lower Central, 059413 Singapore",
     openingTime: "11:00 AM",
     closingTime: "9:30 PM",
-    cusine: { _id: "5c3430ecfc13ae122d000003", name: "Chinese" }
+    cusine: { _id: "5c3430ecfc13ae122d000003", name: "Chinese" },
+    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000009.jpg"
   }
 ];
 
