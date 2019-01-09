b3253500732933127ca92d544ba9fbf736a0d407
Angeline <atan@thoughtworks.com>
2019-01-09 11:40:00 +0800

    Lab 1:  List all restaurants from restaurant service
    - Create Restaurants component to list all restaurants from restaurant service
    - Use bootstrap cards and responsive grid to display the restaurant image, name, cuisine, and opening hours
    - Place an order button in the card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+import Restaurants from "./components/Restaurants";
 
 class App extends Component {
   render() {
-    return <h1>Test Page</h1>;
+    return (
+        <Restaurants />
+    ) 
   }
 }
 
---
+++ src/components/Restaurants.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { getRestaurants } from "../services/restaurantService";
+
+const cardStyle = {
+  width: "20rem",
+};
+
+class Restaurants extends Component {
+  state = {
+    restaurants: getRestaurants()
+  };
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+
+        {this.state.restaurants.map( restaurant => (
+          <div className="col-sm-6 col-md-6 col-lg-4 col-xl-3 d-flex" >
+            
+            <div className="card my-3" style={cardStyle}>
+              <img src={restaurant.imageUrl} className="card-img-top img-fluid"/>
+              <div className="card-body">
+                <h5 className="card-title">{restaurant.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{restaurant.cusine.name}</h6>
+                <p className="card-text"> {restaurant.openingTime} - {restaurant.closingTime}</p>
+              </div>
+
+              <div class="card-footer text-muted">
+                <a href="#" className="btn btn-primary btn-sm">Order</a>
+              </div>
+
+            </div>  
+          </div>
+        ))} 
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Restaurants
