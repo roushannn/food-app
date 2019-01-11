27e63ca1900dd8fc5911ff116f1ef6dd558b5b28
Angeline <atan@thoughtworks.com>
2019-01-11 16:40:11 +0800

    Add test for Restaurants component
    - Check that the restaurant service is called
    - Mock the restaurant service to return 2 restaurants
    - Check that 2 restaurants are displayed after render

---
+++ src/components/Restaurants/Restaurants.test.js
@@ -0,0 +1,41 @@
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+import React from "react";
+import { render } from "react-testing-library";
+import Restaurants from "./Restaurants";
+import * as RestaurantService from "../../services/restaurantService";
+
+const sampleData = [{
+  _id: "5c342ac9fc13ae39f8000000",
+  name: "The Burger Bar by Fatboy's Concepts (Boat Quay)",
+  address: "35 Boat Quay, 049824 Singapore",
+  openingTime: "11:00 AM",
+  closingTime: "10:30 PM",
+  cusine: { _id: "5c3430ecfc13ae122d000000", name: "Western" },
+  imageUrl: "images/restaurants/5c342ac9fc13ae39f8000000.jpg"
+
+},
+{
+  _id: "5c342ac9fc13ae39f8000001",
+  name: "The Soup Spoon (Tanjong Pagar Exchange)",
+  address: "120 Maxwell Road, Tanjong Pagar Xchange, B1-31, 069119 Singapore",
+  openingTime: "12:00 PM",
+  closingTime: "9:30 PM",
+  cusine: { _id: "5c3430ecfc13ae122d000000", name: "Western" },
+  imageUrl: "images/restaurants/5c342ac9fc13ae39f8000001.jpg"
+}]
+
+beforeEach(() => {
+  jest.spyOn(RestaurantService, "getRestaurants").mockImplementation(() => sampleData)
+})
+
+afterEach(() => {
+  RestaurantService.getRestaurants.mockRestore()
+})
+
+test('displays list of two restaurants on load', () => {
+  const {getAllByText} = render(<Restaurants />)
+  
+  expect(RestaurantService.getRestaurants).toHaveBeenCalledTimes(1);
+  expect(getAllByText('Order').length).toEqual(2);
+})
