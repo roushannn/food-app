7e75f5a3c154a123dcaf278872d12320ca153cd5
Angeline <atan@thoughtworks.com>
2019-01-11 15:40:22 +0800

    Remove redundant data from the test. Re arrange order of assertions.

--- src/components/Restaurants/Restaurant.test.js
+++ src/components/Restaurants/Restaurant.test.js
@@ -7,18 +7,17 @@ import Restaurant from "./Restaurant";
 test("renders a restaurant with an image, name, cusine, and opening hours", () => {
   const details = {
     name: "The Burger Bar by Fatboy's Concepts (Boat Quay)",
-    address: "35 Boat Quay, 049824 Singapore",
     openingTime: "11:00 AM",
     closingTime: "10:30 PM",
     cusine: { name: "Western" },
     imageUrl: "images/restaurants/5c342ac9fc13ae39f8000000.jpg"
   };
   const { getByText, getByAltText } = render(<Restaurant details={details} />);
-  expect(getByText(/The Burger Bar by Fatboy/i)).toBeInTheDocument();
-  expect(getByText(/western/i)).toBeInTheDocument();
-  expect(getByText(/11:00 AM.*10:30 PM/i)).toBeInTheDocument();
   expect(getByAltText("restaurant image")).toHaveAttribute(
     "src",
     details.imageUrl
   );
+  expect(getByText(/The Burger Bar by Fatboy/i)).toBeInTheDocument();
+  expect(getByText(/western/i)).toBeInTheDocument();
+  expect(getByText(/11:00 AM.*10:30 PM/i)).toBeInTheDocument();
 });
