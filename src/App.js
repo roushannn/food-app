769f5c72919aec753883d164f6ae06d2e7d071cf
Angeline <atan@thoughtworks.com>
2019-01-15 17:35:27 +0800

    Create a bootstrap NavBar and with links to Home My Orders and Admin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import HomePage from "./components/HomePage/HomePage";
+import NavBar from './components/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
+      <div>
+        <NavBar />
         <HomePage />
-    ) 
+      </div>
+    );
   }
 }
 
