247b864dc343a6feeb4977de0ba76fc14ffe13b0
Angeline <atan@thoughtworks.com>
2019-01-16 11:46:35 +0800

    Convert admin page to a class component as state is required

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, {Component} from "react";
 import Table from "../Table/Table"
 
-function AdminPage() {
-  return (
-    <div data-testid="admin-page">
-      <Table />
-    </div>
-  )
+class AdminPage extends Component {
+  render(){
+    return (
+      <div data-testid="admin-page">
+        <Table />
+      </div>
+    )
+  }
 }
 
 export default AdminPage;
