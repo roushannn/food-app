5dac4c4943e4cca9dfbcbe23dde6b64ad1f103ee
Angeline <atan@thoughtworks.com>
2019-02-15 16:44:43 +0800

    Lab 2: Filter restaurant list by cuisine
    - Create a button group at the top and aligned center
    - Use this for filtering the restaurant list below

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -9,7 +9,18 @@ class HomePage extends Component {
   render() {
     const { restaurants } = this.state;
     return (
-      <div className="container-fluid">
+      <div className="container">
+        <div className="row">
+            <div className="col-4 mx-auto mt-3">
+              <div className="btn-group" role="group" aria-label="Button group with nested dropdown">
+                <button type="button" className="btn btn-primary">All</button>
+                <button type="button" className="btn btn-outline-primary">Western</button>
+                <button type="button" className="btn btn-outline-primary">Chinese</button>
+                <button type="button" className="btn btn-outline-primary">Japanese</button>
+                <button type="button" className="btn btn-outline-primary">Thai</button>
+              </div>
+            </div>
+        </div>
         <div className="row">
           {restaurants.map(restaurant => (
             <div className="col-sm-6 col-md-6 col-lg-4 col-xl-3 d-flex" key={restaurant._id}>
