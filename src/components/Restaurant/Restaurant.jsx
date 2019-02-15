520bfeadd2231a85571d4f6fe692bc388d1eef70
Angeline <atan@thoughtworks.com>
2019-02-15 16:23:33 +0800

    Fix formatting with prettier

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import './Restaurant.scss'
+import React from "react";
+import "./Restaurant.scss";
 
 function Restaurant(props) {
-  const {details} = props
+  const { details } = props;
   return (
-    <div className="card my-3" >
-      <img src={details.imageUrl} className="card-img-top img-fluid" alt="restaurant"/>
+    <div className="card my-3">
+      <img
+        src={details.imageUrl}
+        className="card-img-top img-fluid"
+        alt="restaurant"
+      />
       <div className="card-body">
         <h5 className="card-title">{details.name}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">{details.cuisine.name}</h6>
-        <p className="card-text"> {details.openingTime} - {details.closingTime}</p>
+        <h6 className="card-subtitle mb-2 text-muted">
+          {details.cuisine.name}
+        </h6>
+        <p className="card-text">
+          {" "}
+          {details.openingTime} - {details.closingTime}
+        </p>
       </div>
 
       <div className="card-footer text-muted">
-        <a href="#" className="btn btn-primary btn-sm">Order</a>
+        <a href="#" className="btn btn-primary btn-sm">
+          Order
+        </a>
       </div>
-    </div>  
-  )
+    </div>
+  );
 }
 
-export default Restaurant
+export default Restaurant;
