9b7dc7ae32f8d706da0bcb54dbaadc6f19481d2e
Angeline <atan@thoughtworks.com>
2019-06-12 14:37:27 +0800

    Remove all tests, but leaving app code, for testing lab

--- src/components/HomePage/HomePage.test.js
+++ src/components/HomePage/HomePage.test.js
@@ -1,93 +1,9 @@
-import "jest-dom/extend-expect";
-import "react-testing-library/cleanup-after-each";
-import React from "react";
-import { render, fireEvent } from "react-testing-library";
-import HomePage from "./HomePage";
-import * as RestaurantService from "../../services/restaurantService";
+test("displays list of two restaurants on load", () => {});
 
-const sampleData = [
-  {
-    _id: "5c342ac9fc13ae39f8000000",
-    name: "Burger Bar by Fatboy's Concepts (Boat Quay)",
-    address: "35 Boat Quay, 049824 Singapore",
-    openingTime: "11:00 AM",
-    closingTime: "10:30 PM",
-    cuisine: { _id: "5c3430ecfc13ae122d000000", name: "Western" },
-    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000000.jpg",
-    averagePrice: 25
-  },
-  {
-    _id: "5c342ac9fc13ae39f8000003",
-    name: "Ramen Champion",
-    address:
-      "#03-89, 6 Eu Tong Sen Street, The Central, Clarke Quay, Lower Central, 059817 Singapore",
-    openingTime: "11:00 AM",
-    closingTime: "10:00 PM",
-    cuisine: { _id: "5c3430ecfc13ae122d000001", name: "Japanese" },
-    imageUrl: "images/restaurants/5c342ac9fc13ae39f8000003.jpg",
-    averagePrice: 20
-  }
-];
+test("when western is selected the restaurant list will only show western cuisine", () => {});
 
-beforeEach(() => {
-  jest
-    .spyOn(RestaurantService, "getRestaurants")
-    .mockImplementation(() => sampleData);
-});
+test("when All filter is selected the restaurant list will show all cuisines", () => {});
 
-afterEach(() => {
-  RestaurantService.getRestaurants.mockRestore();
-});
+test("when the page is first loaded restaurants are sorted by name", () => {});
 
-test("displays list of two restaurants on load", () => {
-  const { getAllByText } = render(<HomePage />);
-
-  expect(RestaurantService.getRestaurants).toHaveBeenCalledTimes(1);
-  expect(getAllByText("Order").length).toEqual(2);
-});
-
-test("when western is selected the restaurant list will only show western cuisine", () => {
-  const { getAllByText, getByTestId, debug } = render(<HomePage />);
-  const filterBtnWestern = getByTestId("filter-btn-western");
-  expect(getAllByText("Order").length).toEqual(2);
-
-  fireEvent.click(filterBtnWestern);
-
-  expect(getAllByText("Order").length).toEqual(1);
-});
-
-test("when All filter is selected the restaurant list will show all cuisines", () => {
-  const { getAllByText, getByTestId } = render(<HomePage />);
-  const filterBtnWestern = getByTestId("filter-btn-western");
-  const filterBtnAll = getByTestId("filter-btn-all");
-
-  fireEvent.click(filterBtnWestern);
-  fireEvent.click(filterBtnAll);
-
-  expect(getAllByText("Order").length).toEqual(2);
-});
-
-test("when the page is first loaded restaurants are sorted by name", () => {
-  const { getAllByTestId } = render(<HomePage />);
-
-  expect(getAllByTestId("restaurant-card-title").length).toEqual(2);
-  const first = getAllByTestId("restaurant-card-title")[0];
-  const second = getAllByTestId("restaurant-card-title")[1];
-
-  expect(first).toHaveTextContent(/Burger Bar by Fatboy/i);
-  expect(second).toHaveTextContent(/Ramen Champion/i);
-});
-
-test("when 'Sort by price' is selected the restaurant list will order by price in ascending order", () => {
-  const { getByTestId, getAllByTestId } = render(<HomePage />);
-  const select = getByTestId("sort-by-select");
-
-  fireEvent.change(select, { target: { value: "averagePrice" } });
-
-  const first = getAllByTestId("restaurant-card-title")[0];
-  const second = getAllByTestId("restaurant-card-title")[1];
-
-  expect(getAllByTestId("restaurant-card-title").length).toEqual(2);
-  expect(first).toHaveTextContent(/Ramen Champion/i);
-  expect(second).toHaveTextContent(/Burger Bar by Fatboy/i);
-});
+test("when 'Sort by price' is selected the restaurant list will order by price in ascending order", () => {});
--- src/components/RestaurantForm/RestaurantForm.test.js
+++ src/components/RestaurantForm/RestaurantForm.test.js
@@ -1,67 +1,7 @@
-import "jest-dom/extend-expect";
-import "react-testing-library/cleanup-after-each";
-import React from "react";
-import { render, fireEvent } from "react-testing-library";
-import RestaurantForm from "./RestaurantForm";
+test("displays all form fields on load", () => {});
 
-test("displays all form fields on load", () => {
-  const match = {
-    params: { id: 1 }
-  };
-  const { getByLabelText } = render(<RestaurantForm match={match} />);
+test("Save button us disabled on page load", () => {});
 
-  expect(getByLabelText("Name")).toHaveAttribute("type", "text");
-  expect(getByLabelText("Address")).toHaveAttribute("type", "text");
-  expect(getByLabelText("Opening Time")).toHaveAttribute("type", "text");
-  expect(getByLabelText("Closing Time")).toHaveAttribute("type", "text");
-  expect(getByLabelText("Cuisine")).toBeInTheDocument();
-  expect(getByLabelText("Average Price")).toHaveAttribute("type", "number");
-  expect(getByLabelText("Image URL")).toHaveAttribute("type", "text");
-});
+test("error message appears if text input is invalid", () => {});
 
-test("Save button us disabled on page load", () => {
-  const match = {
-    params: { id: 1 }
-  };
-  const { getByText } = render(<RestaurantForm match={match} />);
-  expect(getByText("Save")).toHaveAttribute("disabled");
-});
-
-test("error message appears if text input is invalid", () => {
-  const match = {
-    params: { id: 1 }
-  };
-  const message = /is not allowed to be empty/i;
-  const { getByText, queryByText, getByLabelText } = render(
-    <RestaurantForm match={match} />
-  );
-  const input = getByLabelText(/name/i);
-
-  //enter invalid input into text field
-  fireEvent.change(input, { target: { value: "a" } });
-  fireEvent.change(input, { target: { value: "" } });
-  expect(getByText(message)).toBeInTheDocument();
-
-  // enter valid input into text field
-  fireEvent.change(input, { target: { value: "Restaurant 1" } });
-  expect(queryByText(message)).not.toBeInTheDocument();
-});
-
-test("error message appears if number input is invalid", () => {
-  const match = {
-    params: { id: 1 }
-  };
-  const message = /must be larger than or equal to 1/i;
-  const { getByText, queryByText, getByLabelText } = render(
-    <RestaurantForm match={match} />
-  );
-  const input = getByLabelText(/Average Price/i);
-
-  //enter invalid input into number field
-  fireEvent.change(input, { target: { value: "0" } });
-  expect(getByText(message)).toBeInTheDocument();
-
-  //enter valid input into number field
-  fireEvent.change(input, { target: { value: "1" } });
-  expect(queryByText(message)).not.toBeInTheDocument();
-});
+test("error message appears if number input is invalid", () => {});
