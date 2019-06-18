6474f4d6d2a4a01e5ab2224d1df0c474f7f22cdb
Angeline <atan@thoughtworks.com>
2019-06-18 18:56:50 +0800

    Update react testing library and imports in tests

--- src/components/HomePage/HomePage.test.js
+++ src/components/HomePage/HomePage.test.js
@@ -1,7 +1,7 @@
 import "jest-dom/extend-expect";
-import "react-testing-library/cleanup-after-each";
+import "@testing-library/react/cleanup-after-each";
 import React from "react";
-import { render } from "react-testing-library";
+import { render } from "@testing-library/react";
 import HomePage from "./HomePage";
 import * as RestaurantService from "../../services/restaurantService";
 
